cfc2ef3e537798157014ad5c6cf3c75b6bc610b9
parkjungyoon <tbr06057@naver.com>
2022-11-30 14:36:32 +0900

    add: 랜덤 숫자 오름차순으로 코드 추가

--- src/main/java/lotto/domain/GenerateRandomNumbersImpl.java
+++ src/main/java/lotto/domain/GenerateRandomNumbersImpl.java
@@ -3,6 +3,7 @@ package lotto.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GenerateRandomNumbersImpl implements GenerateNumbers {
     private static final int RANDOM_MIN_VALUE = 1;
@@ -12,6 +13,7 @@ public class GenerateRandomNumbersImpl implements GenerateNumbers {
     @Override
     public List<Integer> generate() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE, RANDOM_NUMBERS_SIZE);
-        return numbers;
+        List<Integer> sortedLottoNumbers = numbers.stream().sorted().collect(Collectors.toList());
+        return sortedLottoNumbers;
     }
 }
