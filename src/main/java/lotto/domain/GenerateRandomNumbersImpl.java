e87c2764157673154c7fd7b9afb4abb3e287637b
parkjungyoon <tbr06057@naver.com>
2022-11-30 13:39:44 +0900

    feat: 랜덤 숫자 생성하는 기능 구현

---
+++ src/main/java/lotto/domain/GenerateRandomNumbersImpl.java
@@ -0,0 +1,17 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class GenerateRandomNumbersImpl implements GenerateNumbers {
+    private static final int RANDOM_MIN_VALUE = 1;
+    private static final int RANDOM_MAX_VALUE = 45;
+    private static final int RANDOM_NUMBERS_SIZE = 6;
+
+    @Override
+    public List<Integer> generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE, RANDOM_NUMBERS_SIZE);
+        return numbers;
+    }
+}
